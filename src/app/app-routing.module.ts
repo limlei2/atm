ac30a7fefccb977fb176a7f9ffd7b152b8765247
limlei2 <limlei2@outlook.com>
2025-02-28 21:58:35 +0000

    New Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BalanceComponent } from './balance/balance.component';
+import { DepositCoinsComponent } from './deposit-coins/deposit-coins.component';
+import { DepositComponent } from './deposit/deposit.component';
+import { DepositNotesComponent } from './deposit-notes/deposit-notes.component';
+import { WithdrawComponent } from './withdraw/withdraw.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : '', redirectTo: '/balance', pathMatch: 'full'},
+  { path : 'balance', component: BalanceComponent},
+  { path : 'deposit', component: DepositComponent,
+    children: [
+      { path : 'coins', component: DepositCoinsComponent },
+      { path : 'notes', component: DepositNotesComponent }
+    ]
+  },
+  { path : 'withdraw', component: WithdrawComponent},
+  { path : '**', component: PagenotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
